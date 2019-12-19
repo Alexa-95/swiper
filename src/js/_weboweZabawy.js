556f1022e4fdc981c29383f607f536330768b215
Sobańska Aleksandra <aleksandra.sobanska@grupawp.pl>
2019-12-19 15:12:12 +0100

    commit v3

--- src/js/_weboweZabawy.js
+++ src/js/_weboweZabawy.js
@@ -28,13 +28,16 @@ var swiperV = new Swiper('.swiper-container-v', {
 });
 
 
-var vertical = document.querySelectorAll(".swiper-container-v"),
-    horizontal = document.querySelectorAll(".swiper-container-h")
-    slideh = document.querySelectorAll('.swiper-slide')[0];
-    slidev = document.querySelectorAll('.swiper-slide')[1];
+// var vertical = document.querySelectorAll(".swiper-container-v"),
+//     horizontal = document.querySelectorAll(".swiper-container-h")
+//     slideh = document.querySelectorAll('.swiper-slide')[0],
+//     slidev = document.querySelectorAll('.swiper-slide')[1],
+//     dataHistory = document.querySelectorAll('[data-history]'),
+//     dataArray = [];
 
-    // console.log(slide)
-
-if(vertical){
-  slidev = slideh;
-}
+//     for(var i=0; i < dataHistory.length; i++){
+//       dataArray.push(
+//         dataHistory[i].getAttribute("data-history")
+//       )
+//     }
+//     console.log(dataArray)
