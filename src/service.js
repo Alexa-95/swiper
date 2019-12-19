a338ac5ccd2f5a3360be2a15353c06160fdfba09
Sobańska Aleksandra <aleksandra.sobanska@grupawp.pl>
2019-12-19 16:02:40 +0100

    ;_;

--- src/service.js
+++ src/service.js
@@ -160,16 +160,32 @@ var swiperV = new Swiper('.swiper-container-v', {
 });
 
 
-// var vertical = document.querySelectorAll(".swiper-container-v"),
-//     horizontal = document.querySelectorAll(".swiper-container-h")
-//     slideh = document.querySelectorAll('.swiper-slide')[0],
-//     slidev = document.querySelectorAll('.swiper-slide')[1],
-//     dataHistory = document.querySelectorAll('[data-history]'),
-//     dataArray = [];
+var vertical = document.querySelectorAll(".swiper-container-v"),
+    horizontal = document.querySelectorAll(".swiper-container-h")
+    slideh = document.querySelectorAll('.swiper-slide')[0],
+    slidev = document.querySelectorAll('.swiper-slide')[1],
+    dataHistory = document.querySelectorAll('[data-history]');
+    // window.history.pushState('wpisz-cokolwiek-1', 'Title', 'wpisz-cokolwiek-1');
+    
+    // for(var i=0; i < dataHistory.length; i++){
+    //   dataArray.push(
+    //     dataHistory[i].getAttribute("data-history")
+    //   )
+    //   window.history.pushState('wpisz-cokolwiek-4', 'Title', 'wpisz-cokolwiek-4');
+    // }
+    
+    
+    var active = document.querySelectorAll('.swiper-slide-active')
+    if(document.location.pathname.split("/").pop() == ""){
+      window.history.pushState('wpisz-cokolwiek-1', 'Title', 'wpisz-cokolwiek-1');
+    }
+    else{
+      //TO DO
+    }
 
-//     for(var i=0; i < dataHistory.length; i++){
-//       dataArray.push(
-//         dataHistory[i].getAttribute("data-history")
-//       )
-//     }
-//     console.log(dataArray)
+
+
+
+    window.onpopstate = function (event) {
+    }
+    console.log(active)
