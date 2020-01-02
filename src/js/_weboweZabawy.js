b6bdd831695d717f81ef2f955c75b8e07534841d
Aleksandra Sobańska <makowskaleksandra@gmail.com>
2020-01-02 09:27:11 +0100

    stats

--- src/js/_weboweZabawy.js
+++ src/js/_weboweZabawy.js
@@ -6,8 +6,8 @@ var swiperV = new Swiper('.swiper-container-v', {
   mousewheel: true,
   mousewheelControl: true,
   pagination: {
-      el: '.swiper-pagination-v',
-      clickable: true,
+    el: '.swiper-pagination-v',
+    clickable: true,
   },
   hashNavigation: {
     watchState: true,
@@ -16,27 +16,26 @@ var swiperV = new Swiper('.swiper-container-v', {
 );
 var swiperH = new Swiper('.swiper-container-h', {
   spaceBetween: space,
-  // loop: true,
   pagination: {
-      el: '.swiper-pagination-h',
-      clickable: true,
+    el: '.swiper-pagination-h',
+    clickable: true,
   },
   hashNavigation: {
     watchState: true,
   },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
 
 function isDescendant(parent, child) {
   var node = child.parentNode;
   while (node != null) {
-      if (node == parent) {
-          return true;
-      }
-      node = node.parentNode;
+    if (node == parent) {
+      return true;
+    }
+    node = node.parentNode;
   }
   return false;
 }
@@ -44,34 +43,66 @@ function isDescendant(parent, child) {
 window.onpopstate = function () {
 
   var dataHistory = document.querySelectorAll('[data-hash]'),
-      dataId = document.querySelectorAll('[data-id]'),
-      wrapper = document.querySelectorAll('.swiper-wrapper')[0],
-      windowWidth = window.innerWidth;
-    
-      var initSlug = document.location.hash;
-      console.log(initSlug);
+    dataId = document.querySelectorAll('[data-id]'),
+    wrapper = document.querySelectorAll('.swiper-wrapper')[0],
+    windowWidth = window.innerWidth;
 
-      if(!initSlug || initSlug === "#"){
-        wrapper.style.transform = "translate3d(0, 0, 0)";
-        document.location.hash = dataHistory[0].dataset.hash;
+  var initSlug = document.location.hash;
+  console.log(initSlug);
+
+  if (!initSlug || initSlug == "#") {
+    wrapper.style.transform = "translate3d(0, 0, 0)";
+    document.location.hash = dataHistory[0].dataset.hash;
+  }
+  else {
+
+    for (var i = 0; i < dataHistory.length; i++) {
+      if ('#' + dataHistory[i].getAttribute('data-hash') === initSlug) {
+
+        current = document.querySelectorAll('[data-hash]')[i];
+        dataHistory[i].classList.add('swiper-slide-active');
       }
-      else{
-        
-        for(var i=0; i < dataHistory.length; i++){
-          if('#' + dataHistory[i].getAttribute('data-hash') === initSlug){
+    }
 
-            current = document.querySelectorAll('[data-hash]')[i];
-            dataHistory[i].classList.add('swiper-slide-active');
-          }
-        }
-        
-        for(var i=0; i < dataId.length; i++){
-          if(isDescendant(dataId[i], current) == true){
-            console.log('Bla ')
-              wrapper.style.transform = "translate3d(" + (-i*windowWidth - (i*space)) + "px, 0, 0)";
-            }
-          }
-          
-        }
-        
-    };
+    for (var i = 0; i < dataId.length; i++) {
+      if (isDescendant(dataId[i], current) == true) {
+        console.log('Bla ')
+        wrapper.style.transform = "translate3d(" + (-i * windowWidth - (i * space)) + "px, 0, 0)";
+      }
+    }
+  }
+};
+window.onload = function () {
+  var dataHistory = document.querySelectorAll('[data-hash]'),
+    dataId = document.querySelectorAll('[data-id]'),
+    wrapper = document.querySelectorAll('.swiper-wrapper')[0],
+    windowWidth = window.innerWidth;
+
+  var initSlug = document.location.hash;
+  console.log(initSlug);
+
+  if (initSlug) {
+
+    for (var i = 0; i < dataHistory.length; i++) {
+      if ('#' + dataHistory[i].getAttribute('data-hash') === initSlug) {
+
+        current = document.querySelectorAll('[data-hash]')[i];
+        dataHistory[i].classList.add('swiper-slide-active');
+      }
+    }
+
+    for (var i = 0; i < dataId.length; i++) {
+      if (isDescendant(dataId[i], current) == true) {
+        console.log('Bla ')
+        wrapper.style.transform = "translate3d(" + (-i * windowWidth - (i * space)) + "px, 0, 0)";
+      }
+    }
+  }
+};
+
+
+document.querySelectorAll('.swiper-wrapper')[0].addEventListener("transitionend", function (event) {
+  var $arId = document.querySelector('[data-swiper]').getAttribute('data-swiper')
+      $item = document.location.hash
+  console.log("STATS: "+"gallery", "scroll "+ $arId + " / " + $item);
+});
